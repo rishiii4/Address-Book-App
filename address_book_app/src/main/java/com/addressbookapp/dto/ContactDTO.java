b0785b375d36e6edf3db8b1d9d4d19413f787ae5
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-03-01 13:12:00 +0530

    [Pranav] Add. DTO and Model

---
+++ address_book_app/src/main/java/com/addressbookapp/dto/ContactDTO.java
@@ -0,0 +1,9 @@
+package com.addressbookapp.dto;
+
+import lombok.Data;
+
+@Data
+public class ContactDTO {
+    private String name;
+    private String email;
+}
---
+++ address_book_app/src/main/java/com/addressbookapp/model/Contact.java
@@ -0,0 +1,23 @@
+package com.addressbookapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+}

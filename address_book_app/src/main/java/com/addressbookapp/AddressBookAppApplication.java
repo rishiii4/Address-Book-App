bbdff8b9067cfeded6fc7a9441a9ebcd5ddd50c3
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-03-01 12:54:14 +0530

    [Pranav] Add. Database Created

--- address_book_app/src/main/java/com/addressbookapp/AddressBookAppApplication.java
+++ address_book_app/src/main/java/com/addressbookapp/AddressBookAppApplication.java
@@ -3,8 +3,8 @@ package com.addressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
+@SpringBootApplication
+
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {

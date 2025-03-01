5023b26fea5b81d94506714b846f1ea445b7ea12
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-03-01 16:14:33 +0530

    [Pranav] Add. Abilities for Services Layer

--- address_book_app/src/main/java/com/addressbookapp/controller/AddressBookController.java
+++ address_book_app/src/main/java/com/addressbookapp/controller/AddressBookController.java
@@ -1,11 +1,14 @@
 package com.addressbookapp.controller;
 
+import com.addressbookapp.dto.ContactDTO;
+import com.addressbookapp.mapper.ContactMapper;
+import com.addressbookapp.model.Contact;
 import com.addressbookapp.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/addressbook")
@@ -22,5 +25,46 @@ public class AddressBookController {
     @GetMapping
     public ResponseEntity<String> greet(){
         return ResponseEntity.ok("Welcome to Address Book App!");
+    }
+
+    // Adding a contact in List
+    @PostMapping("/add")
+    public ResponseEntity<Contact> addEmployee(@RequestBody ContactDTO contactDTO){
+        return ResponseEntity.ok(contactService.addContact(contactDTO));
+    }
+
+    // Deleting a contact by id
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<String> deleteContact(@PathVariable("id") int id){
+        if(contactService.deleteContact(id)){
+            return ResponseEntity.ok("Deletion Successful!");
+        }else {
+            return ResponseEntity.ok("Id not Found!");
+        }
+    }
+
+    // Getting all contacts
+    @GetMapping("/all")
+    public ResponseEntity<List<Contact>> getAllContacts(){
+        return ResponseEntity.ok(contactService.getAllContact());
+    }
+
+    // Getting a contact by id
+    @GetMapping("get/{id}")
+    public ResponseEntity<Contact> getContactById(@PathVariable("id") int id){
+        return ResponseEntity.ok(contactService.getContactById(id));
+    }
+
+    // Updating a contact by id
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Contact> updateContact(@PathVariable("id") int id, @RequestBody ContactDTO contactDTO){
+        Contact update = contactService.updateContact(id, contactDTO);
+        if(update != null){
+            ContactDTO updateDTO = ContactMapper.INSTANCE.toDto(update);
+            return ResponseEntity.ok(update);
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }
--- address_book_app/src/main/java/com/addressbookapp/service/ContactService.java
+++ address_book_app/src/main/java/com/addressbookapp/service/ContactService.java
@@ -12,5 +12,52 @@ import java.util.List;
 @Service
 @Slf4j
 public class ContactService {
+    private final List<Contact> contactList = new ArrayList<>();
+
+    public Contact addContact(ContactDTO contactDTO) {
+        Contact contact = ContactMapper.INSTANCE.toEntity(contactDTO);
+        contact.setId(contactList.size()+1);
+        log.info("Adding Employee: {}", contactDTO.getName());
+        contactList.add(contact);
+        return contact;
+    }
+
+    public boolean deleteContact(int id){
+        log.info("Deleting an Employee by his Id");
+        for(Contact con : contactList){
+            if(id == con.getId()){
+                contactList.remove(con);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Contact> getAllContact(){
+        log.info("Fetching all employees");
+        return contactList;
+    }
+
+    public Contact getContactById(int id){
+        log.info("Getting an Employee by his Id");
+        for(Contact con : contactList){
+            if(id == con.getId()){
+                return con;
+            }
+        }
+        return null;
+    }
+
+    public Contact updateContact(int id, ContactDTO contactDTO){
+        log.info("Employee Update Process Started");
+        for (Contact contact:contactList){
+            if(id == contact.getId()){
+                contact.setName(contactDTO.getName());
+                contact.setEmail(contactDTO.getEmail());
+                return contact;
+            }
+        }
+        return null;
+    }
 
 }
